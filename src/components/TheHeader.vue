224e0e4143b19bd13a10571ccf494b8d03363b11
Matheus Morais <morais629@gmail.com>
2023-02-16 22:54:10 -0300

    first commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/valid-template-root -->
+<template>
+    <div class="header">
+        Header
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+    .header{
+        background: #000;
+        color: #fff;
+    }
+</style>
